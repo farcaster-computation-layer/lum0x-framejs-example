f44e6f508a5902c2f44c2372ff0be5e697bf7692
Taehee Yoon <th.yoon.dev@gmail.com>
2024-09-13 17:16:27 +0900

    feat : add cover image

--- app/lum0x/route.tsx
+++ app/lum0x/route.tsx
@@ -5,6 +5,7 @@ import {
   getSortedChannelsByReactions,
   lum0x,
 } from "./lum0x";
+import { appURL } from "../utils";
 
 const frameHandler = lum0x(async (ctx) => {
   const message = ctx.message;
@@ -33,36 +34,37 @@ const frameHandler = lum0x(async (ctx) => {
   }
 
   return {
-    image: (
-      <div tw="flex flex-col">
-        <div tw="flex">
-          Find the channels where a specific FID is most active
-        </div>
-        {message?.inputText && <div tw="flex">{`targetFid: ${fid}`}</div>}
-        {message && <div tw="flex">{`requesterFid: ${executedFid}`}</div>}
-        <div tw="flex">
-          <div tw="flex flex-col">
-            {state.sortedChannels
-              ?.slice(0, 3)
-              .map(([channelId, reactionData]: any) => (
-                <div key={channelId} tw="flex flex-col">
-                  <div tw="flex"> {channelId}</div>
-                  <div tw="flex"> Total Reactions: {reactionData.total}</div>
-                  <div tw="flex">
-                    {Object.keys(reactionData)
-                      .filter((type) => type !== "total")
-                      .map((reactionType) => (
-                        <span key={reactionType}>
-                          {reactionType}: {reactionData[reactionType]}
-                        </span>
-                      ))}
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-    ),
+    image: `${appURL()}/lum0x/cover1.png`,
+    // image: (
+    //   <div tw="flex flex-col">
+    //     <div tw="flex">
+    //       Find the channels where a specific FID is most active
+    //     </div>
+    //     {message?.inputText && <div tw="flex">{`targetFid: ${fid}`}</div>}
+    //     {message && <div tw="flex">{`requesterFid: ${executedFid}`}</div>}
+    //     <div tw="flex">
+    //       <div tw="flex flex-col">
+    //         {state.sortedChannels
+    //           ?.slice(0, 3)
+    //           .map(([channelId, reactionData]: any) => (
+    //             <div key={channelId} tw="flex flex-col">
+    //               <div tw="flex"> {channelId}</div>
+    //               <div tw="flex"> Total Reactions: {reactionData.total}</div>
+    //               <div tw="flex">
+    //                 {Object.keys(reactionData)
+    //                   .filter((type) => type !== "total")
+    //                   .map((reactionType) => (
+    //                     <span key={reactionType}>
+    //                       {reactionType}: {reactionData[reactionType]}
+    //                     </span>
+    //                   ))}
+    //               </div>
+    //             </div>
+    //           ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // ),
     textInput: "Input target FID",
     buttons: [
       <Button
